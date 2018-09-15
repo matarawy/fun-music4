94076af4bfeca991da36af7e5fd74e2341a5fadb
matarawy <34127291+matarawy@users.noreply.github.com>
2018-09-15 16:42:22 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,7 +9,7 @@ const queue = new Map();
 const client = new Discord.Client();
 
 
-const prefix = "5";
+const prefix = "4";
 client.on('message', async msg => { // eslint-disable-line
 	if (msg.author.bot) return undefined;
 	
